a3151a747c30f7179f2a830361e731891b51f7ed
wolffanil2 <wolffanil@mail.ru>
2023-04-25 10:18:23 +0400

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
+import Tab from "./Tab";
+import FilePicker from "./FilePicker";
+
+export { 
+    CustomButton,
+    ColorPicker,
+    AIPicker,
+    Tab,
+    FilePicker
+}
